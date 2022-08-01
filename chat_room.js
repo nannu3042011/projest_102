7a1634fc08cdc8a178880b31be3711dc546c9d21
nannu3042011 <86117297+nannu3042011@users.noreply.github.com>
2022-08-01 19:43:54 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBGdO5t3kyReqxqwDp7eBSxJi0oDFGc75k",
+    authDomain: "noor-b136f.firebaseapp.com",
+    databaseURL: "https://noor-b136f-default-rtdb.firebaseio.com",
+    projectId: "noor-b136f",
+    storageBucket: "noor-b136f.appspot.com",
+    messagingSenderId: "317705203410",
+    appId: "1:317705203410:web:d3c9ea410cbc41ddb4c70b"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
